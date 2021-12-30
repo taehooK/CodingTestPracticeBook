b395f6fca29d2c4a165ca4f0ce963528035b973f
star2usemap <star2usemap@gmail.com>
2021-12-30 20:31:30 +0900

    1

--- Code/Chapter11/1.py
+++ Code/Chapter11/1.py
@@ -14,7 +14,7 @@ while i < count: #4. 모험가의 수만큼 반복한다.
     countInGroup += 1#4.2. 그룹내 인원을 센다.
     if countInGroup >= maxFear: #4.3. 그룹내 인원이 최대 공포도보다 크거나 같으면
         countOfGroup += 1#4.3.1. 그룹 수를 센다.
-        countInGroup = 0#4.3.2. 그룹내 인원을 초기화한다.
+        countInGroup = 0 #4.3.2. 그룹내 인원을 초기화한다.
     i += 1
 
 print(countOfGroup)
