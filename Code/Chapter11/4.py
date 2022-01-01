00e4fe2d1d44500c7cba236500b11908c4686e62
star2usemap <star2usemap@gmail.com>
2022-01-02 00:52:40 +0900

    4

---
+++ Code/Chapter11/4.py
@@ -0,0 +1,24 @@
+
+
+count = int(input())
+coins = list(map(int, input().split()))
+list.sort(coins)
+
+priceSet = set([0])
+
+for coin in coins:
+    priceList = list(priceSet)
+    for price in priceList:
+        sum = coin + price
+        priceSet.add(sum)
+
+
+maxPrice = max(priceSet)
+i = 1
+while i < maxPrice:
+    if i not in priceSet:
+        break
+    i += 1
+
+minPriceSum = i
+print(minPriceSum)
