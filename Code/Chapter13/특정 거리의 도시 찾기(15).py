2fa71937771d3aa1542c02b6fe49487d21ffafef
star2usemap <star2usemap@gmail.com>
2022-01-12 17:32:19 +0900

    13-1

---
+++ Code/Chapter13/특정 거리의 도시 찾기(15).py
@@ -0,0 +1,45 @@
+from collections import deque
+import sys
+
+def bfs(graph, start, k):
+    queue = deque()
+    queue.append(start)
+    iLevelInsertCount = 1
+    visited = dict()
+    visited[start] = True
+
+    j = 1
+    while queue and j <= k:
+        count = iLevelInsertCount
+        iLevelInsertCount = 0
+        i = 0
+        while i < count:
+            neighbor = graph[queue.popleft()]
+            for neighborIndex in neighbor:
+                if neighborIndex not in visited:
+                    visited[neighborIndex] = True
+                    queue.append(neighborIndex)
+                    iLevelInsertCount += 1
+            i += 1
+        j += 1
+
+    ret = list(queue)
+    ret.sort()
+
+    return ret
+
+n, m, k, x = map(int, input().split())
+
+graph = [[] for _ in range(n + 1)]
+
+for i in range(m):
+    index, target = map(int, sys.stdin.readline().rstrip().split())
+    graph[index].append(target)
+
+ret = bfs(graph, x, k)
+
+if len(ret) > 0:
+    for i in ret:
+        print(i)
+else:
+    print(-1)
