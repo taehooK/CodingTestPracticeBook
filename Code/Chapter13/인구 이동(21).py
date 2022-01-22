70b6c8efe61ce29aaa118188c6a00d4161bb5ae2
star2usemap <star2usemap@gmail.com>
2022-01-22 21:20:25 +0900

    13-7 수정

--- Code/Chapter13/인구 이동(21).py
+++ Code/Chapter13/인구 이동(21).py
@@ -1,4 +1,5 @@
 import sys
+from collections import deque
 
 rowOffset = [-1, 1, 0, 0]
 columnOffset = [0, 0, -1, 1]
@@ -6,61 +7,67 @@ columnOffset = [0, 0, -1, 1]
 limit_number = 10**6
 sys.setrecursionlimit(limit_number)
 
-def dfs(area, union, temp, row, column):
-    global l, r
-    temp[row][column] = 1
-    union.append((row, column))
+def dfs(area, union, visited, row, column):
+    global l, r, n
+    sum = area[row][column]
 
     for i in range(4):
         nextRow = row + rowOffset[i]
         nextColumn = column + columnOffset[i]
 
-        if 0 <= nextRow < len(area) and 0 <= nextColumn < len(area) and temp[nextRow][nextColumn] == -1:
+        if 0 <= nextRow < len(area) and 0 <= nextColumn < len(area) and (nextRow, nextColumn) not in visited:
             diff = abs(area[row][column] - area[nextRow][nextColumn])
             if l <= diff <= r:
-                dfs(area, union, temp, nextRow, nextColumn)
-    if len(union) <= 1:
-        temp[row][column] = -1
-        del union[0]
+                visited[(nextRow, nextColumn)] = 1
+                union.append((nextRow, nextColumn))
+                sum += dfs(area, union, visited, nextRow, nextColumn)
 
+    return sum
 
-def solution(area):
+def solution(area, n):
+    associations = deque()
+    visited = dict()
+    average_deque = deque()
     ret = 0
     while True:
-        associations = []
-        temp = [[-1] * len(area) for _ in range(len(area))]
-        for i in range(len(area)):
-            for j in range(len(area)):
-                union = []
-                if temp[i][j] == -1:
-                    dfs(area, union, temp, i, j)
-                if len(union) > 0:
-                    associations.append(union)
+        for i in range(n):
+            j = i % 2
+            while j < n:
+                if (i,j) not in visited:
+                    visited[(i,j)] = 1
+                    union = deque()
+                    union.append((i, j))
+                    sum = dfs(area, union, visited, i, j)
+                    if len(union) > 1:
+                        associations.append(union)
+                        average_deque.append(sum // len(union))
+                j += 2
 
         if len(associations) > 0:
             for union in associations:
-                sum = 0
-                for coordinate in union:
-                    sum += area[coordinate[0]][coordinate[1]]
-                average = sum // len(union)
+                average = average_deque.popleft()
                 for coordinate in union:
                     area[coordinate[0]][coordinate[1]] = average
             ret += 1
         else:
             break
+        associations.clear()
+        visited.clear()
+        average_deque.clear()
 
     return ret
 
-
+def convert_to_index(row, column, length):
+    return row * length + column
 def get_input():
     n, l, r = map(int, input().split())
     area = []
     for i in range(n):
         area.append(list(map(int, sys.stdin.readline().rstrip().split())))
 
-    return area, l, r
+    return n, area, l, r
 
 
 if __name__ == '__main__':
-    area, l, r = get_input()
-    print(solution(area))
+    n, area, l, r = get_input()
+    print(solution(area, n))
