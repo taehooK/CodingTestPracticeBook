57899790d78abe740c5999695a1cab1864653795
star2usemap <star2usemap@gmail.com>
2022-01-27 18:17:42 +0900

    15-1 수정

--- Code/Chapter15/정렬된 배열에서 특정 수의 개수 구하기.py
+++ Code/Chapter15/정렬된 배열에서 특정 수의 개수 구하기.py
@@ -1,32 +1,47 @@
-def binary_search(array, value):
+import time
+
+def binary_search_left(array, value):
     start = 0
     end = len(array) - 1
 
-    while start < end:
+    while start <= end:
+        if array[start] == value:
+            return start
+
         middle = (start + end) // 2
-        if array[middle] == value:
+        if array[middle] == value and middle > 0 and array[middle - 1] != value:
             return middle
-        elif array[middle] > value:
+        elif value > array[middle]:
+            start = middle + 1
+        else:
+            end = middle - 1
+    return -1
+
+
+def binary_search_right(array, value):
+    start = 0
+    end = len(array) - 1
+
+    while start <= end:
+        if array[end] == value:
+            return end
+
+        middle = (start + end) // 2
+        if array[middle] == value and middle < len(array) and array[middle + 1] != value:
+            return middle
+        elif value >= array[middle]:
             start = middle + 1
         else:
             end = middle - 1
     return -1
 
 def solution(array, number):
-    index = binary_search(array, number)
-    if index == -1:
-        return -1
-    count = 1
-    i = index - 1
-    while i >= 0 and array[i] == number:
-        count += 1
-        i -= 1
-    i = index + 1
-    while i < len(array) and array[i] == number:
-        count += 1
-        i += 1
+    startIndex = binary_search_left(array, number)
+    endIndex = binary_search_right(array, number)
 
-    return count
+    if startIndex == -1:
+        return -1
+    return endIndex - startIndex + 1
 
 def get_input():
     count, x = map(int, input().split())
@@ -36,6 +51,5 @@ def get_input():
 
 array, x = get_input()
 print(solution(array, x))
-
 
 
