f6ac79c8b68f51660fb51fa896341822685ee8c6
star2usemap <star2usemap@gmail.com>
2022-02-15 23:17:46 +0900

    16-4

---
+++ Code/Chapter16/병사 배치하기(34).py
@@ -0,0 +1,13 @@
+def solution(array, n):
+    dp = [1] * n
+
+    for i in range(1, n):
+        for j in range(0, i):
+            if array[i] < array[j]:
+                dp[i] = max(dp[i], dp[j] + 1)
+
+    return n - max(dp)
+
+n = int(input())
+array = list(map(int, input().split()))
+print(solution(array, n))
