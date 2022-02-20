5cc4573d763b37005d6ad5d62e845c3619f927b3
star2usemap <star2usemap@gmail.com>
2022-02-20 21:43:59 +0900

    17-2

---
+++ Code/Chapter 17/정확한 순위(38).py
@@ -0,0 +1,43 @@
+import sys
+
+def solution():
+    n, m = map(int, input().split())
+
+    INF = int(1e9)
+
+    graph = [[INF] * (n + 1) for _ in range(n + 1)]
+
+    for i in range(1, n + 1):
+        graph[i][i] = 0
+
+    for i in range(m):
+        start, end = map(int, sys.stdin.readline().rstrip().split())
+        graph[start][end] = 1
+
+    for k in range(1, n + 1):
+        for i in range(1, n + 1):
+            for j in range(1, n + 1):
+                graph[i][j] = min(graph[i][j], graph[i][k] + graph[k][j])
+
+    counts = [0] * (n + 1)
+
+    for i in range(1, n + 1):
+        for j in range(1, n + 1):
+            if graph[i][j] > 0 and graph[i][j] < INF:
+                counts[i] += 1
+            if graph[j][i] > 0 and graph[j][i] < INF:
+                counts[i] += 1
+
+    ret = 0
+    for count in counts:
+        if count == n - 1:
+            ret += 1
+
+    return ret
+
+print(solution())
+
+
+
+
+
